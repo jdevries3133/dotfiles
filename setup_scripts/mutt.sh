febe3856ec8da82300a82a78aee234378b89abd7
Jack DeVries <jdevries3133@gmail.com>
2021-08-16 12:00:06 -0400

    small fixes to mutt conf

--- setup_scripts/mutt.sh
+++ setup_scripts/mutt.sh
@@ -2,7 +2,7 @@
 
 # just make symlinks to conf files.
 
-mkdir $HOME/.mutt
+mkdir -p $HOME/.mutt
 
 ln -s $(pwd)/muttrc $HOME/.muttrc
 ln -s $(pwd)/mutt_view_attachment.sh $HOME/.mutt/view_attachment.sh
