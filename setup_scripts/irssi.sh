2a548f3c3a82b56afb31cbf8cabff40e934aaeb5
Jack DeVries <jdevries3133@gmail.com>
2021-08-17 14:43:58 -0400

    make irssi setup script more portable

--- setup_scripts/irssi.sh
+++ setup_scripts/irssi.sh
@@ -10,4 +10,9 @@ cp $(pwd)/irssi_config $config_target
 echo "Enter your nick password for jack__d on LiberaChat"
 read pw
 
-sed -i '' -e "s/{{{ password }}}/$pw/g" "$config_target"
+# different sed implementations on OS X vs Linux :|
+if [[ $OSTYPE == "darwin"* ]] ; then
+    sed -i '' -e "s/{{{ password }}}/$pw/g" "$config_target"
+else
+    sed -i "s/{{{ password }}}/$pw/g" "$config_target"
+fi
