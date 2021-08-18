c1dcdb33e0f75d1d7ba5449fe17b0cf31def3950
Jack DeVries <jdevries3133@gmail.com>
2021-08-18 15:45:46 -0400

    revise mutt setup script

--- setup_scripts/mutt.sh
+++ setup_scripts/mutt.sh
@@ -8,4 +8,7 @@ ln -s $(pwd)/muttrc $HOME/.muttrc
 ln -s $(pwd)/mutt_view_attachment.sh $HOME/.mutt/view_attachment.sh
 ln -s $(pwd)/mailcap $HOME/.mailcap
 
+echo "Thanks!\nJack DeVries" >> $HOME/.mutt/signature_personal
+echo "
+
 vim $(pwd)/mutt_example_client_config
