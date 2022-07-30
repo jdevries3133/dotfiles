3c524b3e86e14cc64defd2489ba36e5254070d56
Jack DeVries <jdevries3133@gmail.com>
2022-07-30 15:07:27 -0400

    simplify life
    * remove vim and neovim from here
    * remove subdir of setup scripts
    * remove dotfiles for things I don't really use
    * get rid of git submodules

--- setup.sh
+++ setup.sh
@@ -1,19 +1,59 @@
 #!/bin/bash
 
-# WARNING: this script is currently untested; see how it goes next time we
-# need to move to a new system.
+# setup homebrew (mac only), git, and zsh for unix-like systems
 
-# this script will only work on the following platforms:
-# - macOS
-# - debian linux (with the apt package manager)
+
+install_warning() {
+    which $1 > /dev/null
+    if [[ $? != 0 ]]; then
+        echo "warning: $1 must be installed"
+    fi
+}
+
 
 if [ "$EUID" -eq 0 ]
   then echo "Do not run this script as root"
   exit 1
 fi
 
-# order matters for some of these, so the sequence is listed explicitly
-[[ $OSTYPE == "darwin"* ]] && bash setup_scripts/install_homebrew.sh
-bash setup_scripts/git.sh
-bash setup_scripts/zsh_setup.sh
-bash setup_scripts/vim.sh
+
+# homebrew
+if [[ $OSTYPE == "darwin"* ]]; then
+    # install homebrew
+    echo "note: homebrew installation will request sudo access"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# git
+ln -s $PWD/gitconfig $HOME/.gitconfig
+mkdir -p $HOME/.config/git
+ln -s $PWD/gitexcludes $HOME/.config/git/ignore
+
+
+# zsh
+ln -s $PWD/zshrc $HOME/.zshrc
+
+which zsh > /dev/null
+if [[ $? != 0 ]] ; then
+    echo "warning: zsh must be installed"
+    echo "re-run the setup script to install oh-my-zsh after installing zsh"
+else
+    # install oh-my-zsh if zsh is already installed
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+
+# tmux
+install_warning tmux
+ln -s $PWD/tmux.conf $HOME/.tmux.conf
+
+
+# mutt
+install_warning mutt
+
+mkdir -p $HOME/.mutt
+ln -s $PWD/muttrc $HOME/.muttrc
+ln -s $PWD/mutt_view_attachment.sh $HOME/.mutt/view_attachment.sh
+ln -s $PWD/mailcap $HOME/.mailcap
+echo "Thanks!
+Jack DeVries" >> $HOME/.mutt/signature_personal
